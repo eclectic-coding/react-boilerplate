d6fe1de14499fc2bda27de82e0f1186f1690542b
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-10-22 20:54:01 -0400

    📦 NEW: Add loader for images in webpack

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -22,6 +22,12 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader'
+        }
       }
     ]
   },
